8e1b797c015f16e1697eef0189478ee58570b4f3
Prateek Kumar <89418989+prateekkumaroriginal@users.noreply.github.com>
2026-04-20 17:16:03 +0530

    feat: add src/lib/utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,11 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
+/**
+ * Combines multiple class names and merges Tailwind CSS classes intelligently
+ * @param inputs - Class values to merge
+ * @returns Merged className string
+ */
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
